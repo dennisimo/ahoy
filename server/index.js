0d8aacb2889198292a825d8cbdd9b6ea8568e4d0
Andy Dennis <andy@dennisimo.com>
2015-03-12 08:44:06 +0000

    updated server/index.js to standard style and removed debugging console.log()

--- server/index.js
+++ server/index.js
@@ -1,18 +1,17 @@
-var ahoy = require('../lib'),
-    static = require('node-static'),
-    file = new static.Server('.', { headers: {'Access-Control-Allow-Origin': '*'} })
+var ahoy = require('../lib')
+var static = require('node-static')
+var file = new static.Server('.', { headers: {'Access-Control-Allow-Origin': '*'} })
 
-require('fs').readFile('ahoy.json', function(err, data) {
-    if (err) {
-        console.log('Error: missing ahoy.json')
-        return false
-    } else {
-console.log(JSON.parse(data))
-        require('http').createServer(function (request, response) {
-            request.addListener('end', function () {
-                file.serve(request, response)
-            }).resume()
-        }).listen(1941)
-        ahoy.broadcast()
-    }
+require('fs').readFile('ahoy.json', function (err, data) {
+  if (err) {
+    console.log('Error: missing ahoy.json')
+    return false
+  } else {
+    require('http').createServer(function (request, response) {
+      request.addListener('end', function () {
+        file.serve(request, response)
+      }).resume()
+    }).listen(1941)
+    ahoy.broadcast()
+  }
 })
